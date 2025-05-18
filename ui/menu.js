5e459d97cecc3be3554a344cedd6a4f75818123a
Francesco Martini <wrCisco@users.noreply.github.com>
2025-05-18 21:19:17 +0200

    Multiple updates in menu factory functions

--- ui/menu.js
+++ ui/menu.js
@@ -8,9 +8,12 @@ const createMenuItemRadioGroup = (label, arr, onclick, onvalidate, horizontal) =
         group.classList.add('simebv-horizontal')
     }
     const map = new Map()
+    let currentValue
+    const current = () => currentValue
     const select = value => {
         if (container.getAttribute('aria-disabled') === 'true') return
         onclick(value)
+        currentValue = value
         const item = map.get(value)
         for (const child of group.children)
             child.setAttribute('aria-checked', child === item ? 'true' : 'false')
@@ -18,7 +21,12 @@ const createMenuItemRadioGroup = (label, arr, onclick, onvalidate, horizontal) =
     const enable = (activate) => {
         activate === false
             ? container.setAttribute('aria-disabled', 'true')
-            : container.setAttribute('aria-disabled', 'false')
+            : container.removeAttribute('aria-disabled')
+    }
+    const visible = (isVisible) => {
+        isVisible === false
+            ? container.style.display = 'none'
+            : container.style.display = null
     }
     const acceptedValues = []
     const validate = onvalidate ?? ((value) => acceptedValues.includes(value))
@@ -26,34 +34,115 @@ const createMenuItemRadioGroup = (label, arr, onclick, onvalidate, horizontal) =
         const item = document.createElement('li')
         item.setAttribute('role', 'menuitemradio')
         item.innerText = label
-        item.onclick = () => select(value)
-        item.onkeydown = (e) => { if (e.key === ' ') select(value) }
-        map.set(value, item)
+        let v
+        if (typeof value === 'string' || typeof value === 'number') {
+            v = value
+        }
+        else {
+            const { val, type, attrs, onchange, prefix = '', suffix = '' } = value
+            const containerInput = document.createElement('span')
+            if (attrs.id)
+                containerInput.id = attrs.id + '-container'
+            const input = document.createElement('input')
+            input.type = type
+            for (const [attr, val] of Object.entries(attrs)) {
+                input.setAttribute(attr, val)
+            }
+            input.onchange = onchange
+            containerInput.append(prefix, input, suffix)
+            item.append(containerInput)
+            v = val
+        }
+        item.onclick = () => select(v)
+        item.onkeydown = (e) => { if (e.key === ' ') select(v) }
+        map.set(v, item)
+        acceptedValues.push(v)
         group.append(item)
-        acceptedValues.push(value)
     }
     container.append(header, group)
-    return { element: container, select, enable, validate }
+    return { element: container, select, enable, validate, current, visible }
 }
 
-const createActionMenuItem = (label, shortcut, onclick, ...args) => {
-    const container = document.createElement('fieldset')
+const createActionMenuItem = (label, shortcut, onclick, container, attrs, ...args) => {
 
     const p = document.createElement('p')
     p.innerText = label
     p.setAttribute('role', 'menuitem')
-    p.setAttribute('aria-haspopup', 'dialog')
+    if (attrs) {
+        for (const [attr, value] of attrs) {
+            p.setAttribute(attr, value)
+        }
+    }
     p.onclick = () => onclick(...args)
 
-    container.append(p)
+    if (!container) {
+        container = document.createElement('fieldset')
+        container.append(p)
+    }
 
-    const s = document.createElement('span')
-    s.innerText = shortcut
-    s.classList.add('simebv-menu-shortcut')
+    if (shortcut) {
+        const s = document.createElement('span')
+        s.innerText = shortcut
+        s.classList.add('simebv-menu-shortcut')
+        p.append(s)
+    }
 
-    p.append(s)
+    const enable = (activate) => {
+        activate === false
+            ? container.setAttribute('aria-disabled', 'true')
+            : container.removeAttribute('aria-disabled')
+    }
 
-    return { element: container, }
+    const visible = (isVisible) => {
+        isVisible === false
+            ? container.style.display = 'none'
+            : container.style.display = null
+    }
+
+    return { element: container, enable, visible }
+}
+
+export const createActionMenuGroup = (label, arr) => {
+    const container = document.createElement('fieldset')
+    const menuGroup = { element: container, items: {} }
+    const header = document.createElement('legend')
+    header.innerText = label
+    const group = document.createElement('ul')
+    group.setAttribute('role', 'presentation')
+    container.append(header, group)
+    for (const item of arr) {
+        const listItem = document.createElement('li')
+        listItem.setAttribute('role', 'menuitem')
+        listItem.innerText = item.label
+        if (item.classList) {
+            listItem.classList.add(...item.classList)
+        }
+        listItem.onclick = () => item.onclick()
+        const select = () => listItem.click()
+        const enable = (active) => {
+            active === false
+                ? listItem.setAttribute('aria-disabled', 'true')
+                : listItem.removeAttribute('aria-disabled')
+        }
+        menuGroup.items[item.name] = {
+            element: listItem, select, enable
+        }
+        group.append(listItem)
+    }
+
+    menuGroup.enable = (activate) => {
+        activate === false
+            ? container.setAttribute('aria-disabled', 'true')
+            : container.removeAttribute('aria-disabled')
+    }
+
+    menuGroup.visible = (isVisible) => {
+        isVisible === false
+            ? container.style.display = 'none'
+            : container.style.display = null
+    }
+
+    return menuGroup
 }
 
 export const createMenu = arr => {
@@ -89,17 +178,20 @@ export const createMenu = arr => {
         currentItem = next
     }
 
-    window.addEventListener('blur', element.hide)
+    // window.addEventListener('blur', element.hide)
     element.addEventListener('click', (e) => e.stopPropagation())
 
-    for (const { name, label, type, items, onclick, onvalidate, horizontal, shortcut } of arr) {
+    for (const { name, label, type, items, onclick, onvalidate, horizontal, shortcut, attrs } of arr) {
         let widget
         switch (type) {
             case 'radio':
-                widget = createMenuItemRadioGroup(label, items, hideAnd(onclick), onvalidate, horizontal)
+                widget = createMenuItemRadioGroup(label, items, onclick, onvalidate, horizontal)
                 break
             case 'action':
-                widget = createActionMenuItem(label, shortcut, hideAnd(onclick))
+                widget = createActionMenuItem(label, shortcut, hideAnd(onclick), undefined, attrs)
+                break
+            case 'group':
+                widget = createActionMenuGroup(label, items)
                 break
             default:
                 null
